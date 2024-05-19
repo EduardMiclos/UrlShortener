9ff37f7514744bac76a8f03923f634c91a0e8a8a
EduardMiclos <micloseduard@yahoo.com>
2024-05-19 23:32:02 +0300

    Created the URLMapping Schema
    The URLMapping is a MongoDB entity representing
    a unique relation between a URL and a short URL, along with
    the 'clicks' property, meaning the total number of times
    the short URL has been accessed.

---
+++ api/models/URLMapping.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const URLMappingSchema = new mongoose.Schema({
+    url: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    shortUrl: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    clicks: {
+        type: int,
+        default: 0
+    }
+}, {timestamps: true})
+
+URLMappingSchema.index({url: 1}, {unique: true});
+URLMappingSchema.index({shortUrl: 1}, {unique: true});
+
+module.exports = mongoose.model('URLMapping', URLMappingSchema);
